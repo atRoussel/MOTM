2eb188149e694d219c96ac22302367e330203b97
Lucas <lucas.drencevic@umontreal.ca>
2020-10-17 19:25:06 +0200

    Sending emails

--- webapp-front-and-back/api/src/main/java/io/takima/demo/Application.java
+++ webapp-front-and-back/api/src/main/java/io/takima/demo/Application.java
@@ -1,23 +1,44 @@
 package io.takima.demo;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-/**
- *
- */
-@ComponentScan
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 @EnableJpaRepositories
-@EnableAutoConfiguration
 @SpringBootConfiguration
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .run();
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Sending Email...");
+        //sendEmail();
+        System.out.println("Done");
+    }
+    void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom("moodofthemonth.epf@gmail.com");
+        msg.setTo("athenais.roussel@epfedu.fr");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("C'est bon ca marche les emails \n Clique sur le lien woula http://localhost:4200/");
+
+        javaMailSender.send(msg);
+
     }
 
 }
