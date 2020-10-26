5d5ac7d5fa2b4e759a8d19f64ec4d19fed37b684
Athénaïs Roussel <athenais.roussel@epfedu.fr>
2020-10-26 10:57:00 +0100

    Debut front

--- webapp-front-and-back/front/src/app/models/comment.model.ts
+++ webapp-front-and-back/front/src/app/models/comment.model.ts
@@ -1,8 +1,10 @@
 import { defaultsDeep } from 'lodash';
+import {Survey} from './survey.model';
 
 export class Comment {
     id: number;
     value: string;
+    survey: Survey;
 
     constructor(comment? : Partial <Comment>) {
         defaultsDeep(this, comment);
