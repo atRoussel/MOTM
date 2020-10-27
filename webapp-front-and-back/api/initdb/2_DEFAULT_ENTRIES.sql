b3a5b564c934fed7458b0e8918e9b62604fe70ba
Lucas <lucas.drencevic@umontreal.ca>
2020-10-27 23:54:42 +0100

    Add connexion (admin = admin@gmail.com)

--- webapp-front-and-back/api/initdb/2_DEFAULT_ENTRIES.sql
+++ webapp-front-and-back/api/initdb/2_DEFAULT_ENTRIES.sql
@@ -8,6 +8,7 @@ INSERT INTO defaultdb.users (id, name, mail, date) VALUES (7, 'Gaston', 'Laplouz
 INSERT INTO defaultdb.users (id, name, mail, date) VALUES (8, 'Gisèle', 'Detable@gmail.com', '01/01/2020');
 INSERT INTO defaultdb.users (id, name, mail, date) VALUES (9, 'Thomas', 'Ouaque@gmail.com', '01/01/2020');
 INSERT INTO defaultdb.users (id, name, mail, date) VALUES (10, 'Sacha', 'Telfriz@gmail.com','01/01/2020');
+INSERT INTO defaultdb.users (id, name, mail, date) VALUES (11, 'Patrick', 'admin@gmail.com','01/01/2020');
 
 INSERT INTO defaultdb.surveys(id, description, title) VALUES (1, 'Mon premier MOTM', 'MOTM de Noël');
 INSERT INTO defaultdb.surveys(id, description, title) VALUES (2, 'Un deuxième pour la route', 'MOTM du Nouvel An');
--- webapp-front-and-back/front/src/app/pages/sondage/sondage.component.ts
+++ webapp-front-and-back/front/src/app/pages/sondage/sondage.component.ts
@@ -7,6 +7,8 @@ import {Observable} from 'rxjs';
 import {AnswerService} from '../../services/answer.service';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
+import {UserService} from '../../services/user.service';
+import {User} from '../../models/user.model';
 
 @Component({
   selector: 'app-sondage',
@@ -17,12 +19,14 @@ export class SondageComponent implements OnInit {
   selectedMood = '';
   myTextarea;
   surveys: Survey[];
+  users: User[];
   lastSurvey: Survey;
-  lastId;
   constructor(private surveyService: SurveyService, private  commentService: CommentService, private answerService: AnswerService,
-              private router: Router, private _location: Location) { }
+              private userService: UserService, private router: Router, private _location: Location) { }
   ngOnInit(): void {
     this.surveyService.getSurveys().subscribe(surveys => this.lastSurvey = surveys[surveys.length-1]);
+    this.userService.getUsers().subscribe(users => this.users = users);
+    document.getElementById('id02').style.display='block';
   }
   onSubmit() {
     const comment = defaultsDeep({
@@ -47,5 +51,12 @@ export class SondageComponent implements OnInit {
   }
   refresh() {
     window.location.reload();
+  }
+  checkEmail(str) {
+    this.users.forEach(user => {
+      if(user.mail === str) {
+        document.getElementById('id02').style.display='none';
+      }
+    })
   }
 }
