039da28ff4b6a9c8d20bb4b3796389acfef86f90
Lucas <lucas.drencevic@umontreal.ca>
2020-10-30 17:00:50 +0100

    Add multiple questions and answer + link to bdd

--- webapp-front-and-back/front/src/app/pages/sondage/sondage.component.ts
+++ webapp-front-and-back/front/src/app/pages/sondage/sondage.component.ts
@@ -21,15 +21,19 @@ export class SondageComponent implements OnInit {
   surveys: Survey[];
   users: User[];
   lastSurvey: Survey;
+  responseGrid: string[];
+
   constructor(private surveyService: SurveyService, private  commentService: CommentService, private answerService: AnswerService,
               private userService: UserService, private router: Router, private _location: Location) { }
   ngOnInit(): void {
     this.surveyService.getSurveys().subscribe(surveys => this.lastSurvey = surveys[surveys.length-1]);
     this.userService.getUsers().subscribe(users => this.users = users);
     document.getElementById('id02').style.display='block';
+    this.responseGrid = [];
   }
-  selectChangeHandler (event: any) {
+  selectChangeHandler (event: any, index: number) {
     this.selectedMood = event.target.value;
+    this.responseGrid[index] = this.selectedMood;
   }
   addComment(commentValue, commentSurvey) {
     const comment = defaultsDeep({
@@ -39,12 +43,14 @@ export class SondageComponent implements OnInit {
     });
     this.commentService.addComment(comment).subscribe(comments => console.log(comments));
 
-    const answer = defaultsDeep({
-      id: null,
-      value: this.selectedMood,
-      question: commentSurvey.questions[0]
-    })
-    this.answerService.addAnswer(answer).subscribe(answers => console.log(answers));
+    this.lastSurvey.questions.forEach((qu, i) =>{
+      const answer = defaultsDeep({
+        id: null,
+        value: this.responseGrid[i],
+        question: commentSurvey.questions[i]
+      })
+      this.answerService.addAnswer(answer).subscribe(answers => console.log(answers));
+    });
   }
   refresh() {
     window.location.reload();
